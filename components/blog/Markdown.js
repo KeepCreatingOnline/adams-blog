76ad8d25d85042ff24656b4b418053ec43cfc378
Keep Creating Online <94152424+KeepCreatingOnline@users.noreply.github.com>
2023-05-15 22:15:36 -0400

    update

--- components/blog/Markdown.js
+++ components/blog/Markdown.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { unified } from 'unified';
 import { remarkParse } from 'remark-parse';
 import dynamic from 'next/dynamic';
-import { remarkGfm } from 'remark-gfm';
+import remarkGfmPlugin from 'remark-gfm'; // Updated import statement
 
 const ReactMarkdown = dynamic(() => import('react-markdown').then((mod) => mod.default));
-const remarkGfmPlugin = dynamic(() => import('remark-gfm').then((mod) => mod.remarkGfm));
+const remarkGfm = dynamic(() => import('remark-gfm').then((mod) => mod.remarkGfm)); // If required, update this line too
 
 export default function Markdown({ content }) {
   const result = unified().use(remarkParse).use(remarkGfmPlugin).processSync(content);

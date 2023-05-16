606388bc19376d193e5776911b68a5f450262cf5
Keep Creating Online <94152424+KeepCreatingOnline@users.noreply.github.com>
2023-05-15 22:22:56 -0400

    update

--- components/blog/Markdown.js
+++ components/blog/Markdown.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { unified } from 'unified';
 import { remarkParse } from 'remark-parse';
-import { remarkGfm } from 'remark-gfm';
+import remark2react from 'remark-react';
+import 'github-markdown-css';
 
 export default function Markdown({ content }) {
-  const result = unified().use(remarkParse).use(remarkGfm).processSync(content);
-  const html = result.toString();
+  const result = unified().use(remarkParse).use(remark2react).processSync(content);
+  const tree = result.result;
 
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  return <div className="markdown-body">{tree}</div>;
 }

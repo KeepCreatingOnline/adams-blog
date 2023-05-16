1c13ea80a5901a3fcfa78e48b869235dd4b4230d
Keep Creating Online <94152424+KeepCreatingOnline@users.noreply.github.com>
2023-05-15 21:07:22 -0400

    update

--- settings/authors.js
+++ settings/authors.js
@@ -9,14 +9,10 @@
 // if the blog doesn't need authors, empty the authors object e.g const authors = {};
 
 const authors = {
-    "adam-malin": {
+    "adam": {
         name: "Adam",
-        description: "Dad"
+        description: "An artist and coder on the web."
     },
-    "jessica-malin": {
-        name: "Jess",
-        description: "Mom"
-    }
 };
 
 export default authors;

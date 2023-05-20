b124a55fab6f0d40d4a2a11fd498df10a9e8e386
thePR0M3TH3AN <94152424+KeepCreatingOnline@users.noreply.github.com>
2023-05-20 19:34:31 -0400

    update

--- components/blog/Markdown.js
+++ components/blog/Markdown.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm'; // import the GFM plugin
+import remarkGfm from 'remark-gfm';
 import 'github-markdown-css';
 
 export default function Markdown({ content }) {
   return (
     <ReactMarkdown 
       className="markdown-body" 
-      children={content} 
-      remarkPlugins={[gfm]} // add the plugin here
+      children={content}
+      remarkPlugins={[remarkGfm]} 
     />
   );
 }
